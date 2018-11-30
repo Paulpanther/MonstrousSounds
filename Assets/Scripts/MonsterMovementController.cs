62e3f69131a68186970735d75141e5dcd0f21ca0
paul.methfessel <paul.methfessel@student.hpi.de>
2018-11-30 20:31:20 +0100

    Added Player Sprite

---
+++ Assets/Scripts/MonsterMovementController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovementController : MonoBehaviour {
+
+    public float playerSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+        }
+
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
