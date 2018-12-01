6b48de09063e0b7844308a1581c6123b1926f07d
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2018-12-01 11:30:28 +0100

    added sounds

---
+++ Assets/SoundManagerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerController : MonoBehaviour
+{
+
+    public static AudioClip stepSound;
+    static AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        stepSound = Resources.Load<AudioClip>("step");
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void playSound(int soundId)
+    {
+        switch (soundId)
+        {
+            case 0:
+                audioSource.PlayOneShot(stepSound);
+                break;
+        }
+    }
+}
