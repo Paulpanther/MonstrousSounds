a985d2e00e30545fac225846070223686bba2b69
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2018-12-01 12:48:17 +0100

    vision is now increasing on walking and decreased when standing

--- Assets/Scripts/MonsterMovementController.cs
+++ Assets/Scripts/MonsterMovementController.cs
@@ -9,6 +9,14 @@ public class MonsterMovementController : MonoBehaviour {
     private void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        if(moveVertical != 0 || moveHorizontal != 0)
+        {
+            ViewSizeScript.setToWalking();
+        }
+        else
+        {
+            ViewSizeScript.setToStanding();
+        }
         transform.Translate(moveHorizontal * playerSpeed, moveVertical * playerSpeed, 0);
     }
 }
---
+++ Assets/ViewSizeScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewSizeScript : MonoBehaviour {
+    private enum Action { Screaming, Standing, Walking};
+
+    private static int screamCount = 0;
+    public static int maxScreamFrames = 120;
+    private static Vector2 standingScaleFactor = new Vector3(0.4f, 0.4f, 1);
+    private static Vector2 walkingScaleFactor = new Vector3(0.6f, 0.6f, 1);
+    private static Vector2 screamScaleFactor = new Vector3(1.4f, 1.4f, 1);
+
+    private static Action currentAction;
+
+    // Use this for initialization
+    void Start () {
+        currentAction = Action.Standing;
+    }
+	
+    public static void setToScreamSize()
+    {
+        if (screamCount == 0) {
+            currentAction = Action.Screaming;
+        }
+    }
+
+    public static void setToWalking()
+    {
+        currentAction = Action.Walking;
+    }
+
+    public static void setToStanding()
+    {
+        currentAction = Action.Standing;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        switch (currentAction)
+        {
+            case Action.Screaming:
+                ++screamCount;
+                if(screamCount >= maxScreamFrames)
+                {
+                    currentAction = Action.Standing;
+                }
+                transform.localScale = screamScaleFactor;
+                break;
+            case Action.Walking:
+                transform.localScale = walkingScaleFactor;
+                break;
+            case Action.Standing:
+                transform.localScale = standingScaleFactor;
+                break;
+        }
+	}
+}
